d6393e4741b3803fe5aa504eb9c9c0422d953bb4
Mauricio Portillo <mauricio.portillo@valkimia.com>
2022-03-11 03:40:35 -0300

    change seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-(7.days.ago.to_date..Date.current).each do |date|
+(3.months.ago.to_date..Date.current).each do |date|
     rand(6).times do
         Expense.create!(date: date, description: Faker::Commerce.product_name, type: Expense.types.keys.sample, amount: Faker::Number.between(from: 10, to: 500))
     end
